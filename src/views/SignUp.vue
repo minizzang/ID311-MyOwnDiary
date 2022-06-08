08b6ba930f9734eede3bb4a2a0c523df7f21c98f
minizzang <minizzang@kaist.ac.kr>
2022-06-09 00:35:50 +0900

    Fix: email valid check error

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -96,7 +96,7 @@ export default {
       var regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
       const result = regex.test(this.email)
       this.setValidEmail(result)
-      return regex.test(result)
+      return result
     },
     validPassword () {
       if (this.isPwValid === null) {
