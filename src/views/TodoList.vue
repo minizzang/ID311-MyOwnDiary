b162457d0e99463fd84e40d4401332d294d2f37e
dpsk9930 <dpsk9930@kaist.ac.kr>
2022-06-12 01:14:30 +0900

    Add checkbox

--- src/views/TodoList.vue
+++ src/views/TodoList.vue
@@ -1,8 +1,9 @@
 <template>
   <section>
-        {{ todoItem }}
     <transition-group name="list" tag="ul" id="todoUl">
-      <li v-for="(todoItem, index) in propsdata" :key="todoItem" id="todoLi">
+      <li v-for="(todoItem, index) in propsdata" :key="todoItem.text + index" id="todoLi">
+        <input type="checkbox" id="checkboxInput" v-bind:value="index" v-bind:checked="todoItem.checked" v-on:change="changed(index)">
+        {{ todoItem.text }}
         <span class="removeBtn" type="button" @click="removeTodo(todoItem, index)">
             <i class="far fa-trash-alt" aria-hidden="true"></i>
         </span>
@@ -13,23 +14,35 @@
 
 <script>
 export default {
+  watch: {
+    propsdata () {
+      console.log('props data watch')
+      this.localTodo = this.propsdata
+    }
+  },
   props: ['propsdata'],
   methods: {
     removeTodo (todoItem, index) {
       this.$emit('removeTodo', todoItem, index)
+    },
+    changed (index) {
+      console.log(index)
+      this.$emit('checkTodo', index)
     }
   }
 }
 </script>
 
 <style>
-.list-enter-active{
+.list-enter-active {
   transition: all 1s;
 }
-.list-enter{
+
+.list-enter {
   opacity: 0;
   transform: translateY(30px);
 }
+
 #todoUl {
   width: 90%;
   margin-left: auto;
@@ -39,6 +52,7 @@ export default {
   margin-top: 10px;
   text-align: left;
 }
+
 #todoLi {
   display: flex;
   min-height: 50px;
@@ -48,8 +62,15 @@ export default {
   padding: 0 0.9rem;
   background: white;
   border-radius: 5px;
+  border: solid 0.1em black;
+  align-items: center;
 }
-.removeBtn{
+
+#checkboxInput {
+  margin-right: 10px;
+}
+
+.removeBtn {
   margin-left: auto;
   color: #DE4343;
 }
