4aa9bc083870b7e67bf0bddc323782f530ebb20a
Yumin Cho <dbals081216@gmail.com>
2022-06-12 06:00:24 +0900

    Fix: Memo with tag

---
+++ src/views/MemoInput.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="todoinput">
+    <input type="text" v-model="tag"/>
+    <input type="text" v-model="content"/>
+    <button type="button" @click="addMemo">ADD</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      tag: '',
+      content: ''
+    }
+  },
+  methods: {
+    addMemo () {
+      this.$emit('addMemo', this.content, this.tag)
+      this.content = ''
+      this.tag = ''
+    }
+  }
+}
+</script>
+
+<style></style>
