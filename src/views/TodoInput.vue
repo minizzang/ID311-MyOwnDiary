6195fe867d5ab7da798b6e25233e523ecf0d2544
Minhee Kim <79495204+minizzang@users.noreply.github.com>
2022-06-11 23:02:56 +0900

    Merge pull request #5 from minizzang/TodoList
    Todo List

---
+++ src/views/TodoInput.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="inputContainer">
+    <input id="inputBox" type="text" v-model="InputTodoItem" placeholder="Type what you have to do!"
+           v-on:keyup.enter="addTodo">
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      InputTodoItem: ''
+    }
+  },
+  methods: {
+    addTodo () {
+      if (this.InputTodoItem !== '') {
+        let value = this.InputTodoItem
+        this.$emit('addTodo', value)
+        this.clearInput()
+      }
+    },
+    clearInput () {
+      this.InputTodoItem = ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+.inputContainer{
+  background: white;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+  align-items: center;
+}
+#inputBox {
+  font-size: 0.9rem;
+  width: 80%;
+  text-align: center;
+}
+</style>
