639530badf6e0f5e511b6d7082c7d71ec3dec437
Yumin Cho <dbals081216@gmail.com>
2022-06-12 16:07:51 +0900

    Fix: Memo style

--- src/views/MemoList.vue
+++ src/views/MemoList.vue
@@ -1,22 +1,12 @@
 <template>
   <div id="memoList">
-    <div id="memoTag" v-for="(tag, index) in tags" v-if="tag != ''" :key="tag.id" class="shadow">
-        <strong class="tagEach">
-            #{{tag}} :
-        </strong>
-        <div class="memoEach" v-for="([content, thistag]) in memos" v-if="(thistag == tag)" :key="content.id">
-            {{ content }}
-            <button type="button" @click="removeMemo(memos, content)">❌</button>
-        </div>
-        <button type="button" id="tagBtn" @click="upTag(tags, index)">⬆️</button>
-        <button type="button" id="tagBtn" @click="downTag(tags, index)">⬇️</button>
-        <button type="button" id="tagBtn" @click="removeTag(memos, tags, index)">🗑</button>
+    <div class="memoEach" v-for="(number) in memos" :key="number.id">
+    <textarea class="shadow memoText"></textarea>
+    <!-- {{number}}th memo -->
+    <div>
+        <button type="button" class="memoButton" @click="removeMemo(memos, number)">x</button>
+        <!-- <button type="button" class="memoButton" @click="saveMemo(memos, number)">✓</button> -->
     </div>
-    <div id="memoTag" v-for="(tag) in tags" v-if="tag == ''" :key="tag.id" class="shadow">
-        <div class="memoEach" v-for="([content, thistag]) in memos" v-if="(thistag == tag)" :key="content.id">
-            {{ content }}
-            <button type="button" @click="removeMemo(memos, content)">❌</button>
-        </div>
     </div>
   </div>
 </template>
@@ -30,56 +20,44 @@ export default {
   methods: {
     removeMemo (memos, index) {
       this.$emit('removeMemo', memos, index)
-    },
-    removeTag (memos, tags, index) {
-      this.$emit('removeTag', memos, tags, index)
-    },
-    downTag (tags, index) {
-      this.$emit('downTag', tags, index)
-    },
-    upTag (tags, index) {
-      this.$emit('upTag', tags, index)
     }
   }
 }
 </script>
 
 <style scoped>
-#tagBtn {
-    margin-left: 1em;
-}
 #memoList {
+  display: flex;
   flex-direction: row;
   justify-content: flex-start;
   align-content: flex-start;
   flex-wrap: wrap;
   overflow: scroll;
-  height: 65vh;
-  padding: 1em
-}
-#memoTag {
-  display: flex;
-  align-content: flex-start;
-  flex-wrap: wrap;
-  overflow: scroll;
-  margin-left: 0.5em;
-  margin-top: 0.5em;
-  padding: 0.5em;
-  border-radius: 10px;
+  height: 60vh;
+  padding: 2em;
+  margin: 1em;
 }
 .memoEach {
- border: 1px solid black;
- width: auto;
- height: auto;
- padding: 0.5em;
- border-radius: 10px;
- margin-right: 0.5em;
+    display: flex;
+    align-content: flex-start;
+    padding: 1em;
 }
-.tagEach {
- width: auto;
- height: auto;
+.memoText {
+ width: 20vh;
+ height: 20vh;
  padding: 0.5em;
  border-radius: 10px;
  margin-right: 0.5em;
+ overflow: scroll;
+}
+.memoButton {
+    height: 20px;
+    width: 20px;
+    margin-bottom: 1vh;
+    border-radius: 100%;
+    background-color: lightsalmon;
+    color: white;
+    text-align: center;
+    line-height: 1em;
 }
 </style>
